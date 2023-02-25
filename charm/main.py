d1948157b99535b729a4b6137619ddd79a8de162
DigiDuncan <digiduncan@gmail.com>
2023-02-25 01:38:52 -0500

    hitbox algo moved

--- charm/main.py
+++ charm/main.py
@@ -73,7 +73,7 @@ class CharmGame(DigiWindow):
             with pkg_resources.path(charm.data.audio, f"error-{soundname}.wav") as p:
                 self.sounds["error-" + soundname] = arcade.Sound(p)
 
-        arcade.hitbox.default_algorithm = arcade.hitbox.bounding.BoundingHitBoxAlgorithm()
+        arcade.hitbox.default_algorithm = arcade.hitbox.algo_bounding_box
 
         self.initial_view = TitleView()
 
