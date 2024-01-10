828db65dc08e57fe75954e4ef0bd3783e4765e0f
DigiDuncan <digiduncan@gmail.com>
2024-01-10 17:57:05 -0500

    clean up the documentation for get_album_art

--- charm/ui/utils.py
+++ charm/ui/utils.py
@@ -1,4 +1,3 @@
-from hashlib import sha1
 from pathlib import Path
 
 import PIL.Image
@@ -10,11 +9,14 @@ from charm.lib.utils import img_from_resource
 
 
 def get_album_art(metadata: Metadata, size = 200) -> Texture:
+    """Get an album art Texture from a song metadata."""
     # Iterate through frankly too many possible paths for the album art location.
     art_path = None
+    # Clone Hero-style (also probably the recommended format.)
     art_paths = [Path(metadata.path / "album.jpg"),
                  Path(metadata.path / "album.png"),
                  Path(metadata.path / "album.gif")]
+    # Stepmania-style
     art_paths.extend(metadata.path.glob("*jacket.png"))
     art_paths.extend(metadata.path.glob("*jacket.gif"))
     art_paths.extend(metadata.path.glob("*jacket.jpg"))
@@ -25,7 +27,6 @@ def get_album_art(metadata: Metadata, size = 200) -> Texture:
 
     if art_path is None:
         # We *still* didn't find one? Fine.
-        # Make sure this directory, like, exists?
         album_art_img = img_from_resource(charm.data.images, "no_image_found.png")
     else:
         album_art_img = PIL.Image.open(art_path)
