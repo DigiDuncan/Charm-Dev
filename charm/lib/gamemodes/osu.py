e06e8f59eaf4fcc4b073fe4e5c992b5f10bd967c
DigiDuncan <digiduncan@gmail.com>
2024-01-18 00:50:31 -0500

    far better osu! parsing

--- charm/lib/gamemodes/osu.py
+++ charm/lib/gamemodes/osu.py
@@ -17,13 +17,14 @@ RE_KV_TYPE_1 = r"(.+):(.+)"
 RE_KV_TYPE_2 = r"(.+): (.+)"
 RE_KV_TYPE_3 = r"(.+) : (.+)"
 RE_TIMING_POINT = fr"({NUM}),({NUM}),({INT}),({INT}),({INT}),({INT}),({ZO}),({INT})$"
+RE_DEFAULT = fr"({INT}),({INT}),({INT}),({INT}),({INT})"
 RE_HIT_SAMPLE = fr"({INT}):({INT}):({INT}):({INT}):(.+)?"
-RE_HIT_CIRCLE = fr"({INT}),({INT}),({INT}),({INT}),({INT})(,{RE_HIT_SAMPLE})?$"
+RE_HIT_CIRCLE = fr"{RE_DEFAULT},({RE_HIT_SAMPLE})$"
 RE_PIPE_SEP_INT = r"\d+(\|\d+)*"
-RE_PIPE_SEP_COLON_SEP_INT = r"(\d+:\d+)(\|\d+:\d+)*"
-RE_SLIDER = fr"({INT}),({INT}),({INT}),({INT}),({INT}),(([BCLP])(\|\d+:\d+)+),({INT}),({NUM}),({RE_PIPE_SEP_INT}),({RE_PIPE_SEP_COLON_SEP_INT})(,{RE_HIT_SAMPLE})?$"
-RE_SPINNER = fr"({INT}),({INT}),({INT}),({INT}),({INT}),({INT}),({RE_HIT_SAMPLE})$"
-RE_HOLD = fr"({INT}),({INT}),({INT}),({INT}),({INT}),({INT}):({RE_HIT_SAMPLE})$"
+RE_PIPE_SEP_COLON_SEP_INT = r"\d+:\d+(?:\|\d+:\d+)*"
+
+RE_SLIDER = fr"{RE_DEFAULT},([BCLP])\|({RE_PIPE_SEP_COLON_SEP_INT}),({INT}),({NUM}),({RE_HIT_SAMPLE})$"
+RE_SPINNER = fr"{RE_DEFAULT},({INT}),({RE_HIT_SAMPLE})$"
 
 logger = logging.getLogger("charm")
 
@@ -264,6 +265,25 @@ class OsuSampleEvent(OsuEvent):
     filename: str
     volume: float = 1
 
+def get_standard_data_from_match(m: re.Match[str]) -> tuple[int, int, int, int, int]:
+    x = int(m.group(1))
+    y = int(m.group(2))
+    time = int(m.group(3)) / 1000
+    object_type = int(m.group(4))
+    hit_sound = int(m.group(5))
+
+    return x, y, time, object_type, hit_sound
+
+def hit_sample_from_match(m: re.Match[str]) -> OsuHitSample:
+    normal_set = int(m.group(1))
+    addition_set = int(m.group(2))
+    index = int(m.group(3))
+    volume = int(m.group(4))
+    filename = m.group(5) or None
+
+    hit_sample = OsuHitSample(normal_set, addition_set, index, volume, filename)
+    return hit_sample
+
 @dataclass
 class RawOsuChart:
     general: OsuGeneralData = field(default_factory=OsuGeneralData)
@@ -343,17 +363,17 @@ class RawOsuChart:
                 if m := re.match(RE_KV_TYPE_1, line):
                     match m.group(1):
                         case "HPDrainRate":
-                            chart.difficulty.hp_drain_rate = m.group(2)
+                            chart.difficulty.hp_drain_rate = float(m.group(2))
                         case "CircleSize":
-                            chart.difficulty.circle_size = m.group(2)
+                            chart.difficulty.circle_size = float(m.group(2))
                         case "OverallDifficulty":
-                            chart.difficulty.overall_difficulty = m.group(2)
+                            chart.difficulty.overall_difficulty = float(m.group(2))
                         case "ApproachRate":
-                            chart.difficulty.approach_rate = m.group(2)
+                            chart.difficulty.approach_rate = float(m.group(2))
                         case "SliderMultiplier":
-                            chart.difficulty.slider_multiplier = m.group(2)
+                            chart.difficulty.slider_multiplier = float(m.group(2))
                         case "SliderTickRate":
-                            chart.difficulty.slider_tick_rate = m.group(2)
+                            chart.difficulty.slider_tick_rate = float(m.group(2))
                         case _:
                             logger.debug(f"Unknown Difficulty metadata '{m.group(2)} (Line {line_num})'.")
             elif current_header == "Events":
@@ -386,113 +406,59 @@ class RawOsuChart:
                 # Ignoring colors for now.
                 pass
             elif current_header == "HitObjects":
-                # x,y,time,type,hitSound
                 # x,y,time,type,hitSound,hitSample
                 if m := re.match(RE_HIT_CIRCLE, line):
-                    x = int(m.group(1))
-                    y = int(m.group(2))
-                    time = int(m.group(3)) / 1000
-                    object_type = int(m.group(4))
-                    hit_sound = int(m.group(5))
+                    x, y, time, object_type, hit_sound = get_standard_data_from_match(m)
+                    # Hit sample
                     hit_sample_data = m.group(6)
-                    if hit_sample_data:
-                        m2 = re.search(RE_HIT_SAMPLE, hit_sample_data)
-                        if m2 is None:
-                            raise ChartParseError(line_num, f"Unparseable hit sample data '{line}'.")
-                        normal_set = int(m2.group(1))
-                        addition_set = int(m2.group(2))
-                        index = int(m2.group(3))
-                        volume = int(m2.group(4))
-                        filename = m2.group(5) or None
-                        hit_sample = OsuHitSample(normal_set, addition_set, index, volume, filename)
-                    else:
-                        hit_sample = OsuHitSample()
-                    chart.hit_objects.append(
-                        OsuHitCircle(x, y, time, object_type, hit_sound, hit_sample)
-                    )
-                # MAKING THE ASSUMPTION THESE REQUIRE HITSAMPLE
-                # OTHERWISE IT WOULD BE INDESCERNABLE FROM A SPINNER
-                # x,y,time,type,hitSound,endTime:hitSample
-                elif m := re.match(RE_HOLD, line):
-                    x = int(m.group(1))
-                    y = int(m.group(2))
-                    time = int(m.group(3)) / 1000
-                    object_type = int(m.group(4))
-                    hit_sound = int(m.group(5))
-                    end_time = int(m.group(6)) / 1000
-                    hit_sample_data = m.group(7)
                     m2 = re.search(RE_HIT_SAMPLE, hit_sample_data)
-                    normal_set = int(m2.group(1))
-                    addition_set = int(m2.group(2))
-                    index = int(m2.group(3))
-                    volume = int(m2.group(4))
-                    filename = m2.group(5) or None
-                    chart.hit_objects.append(
-                        OsuHold(x, y, time, object_type, hit_sound, end_time,
-                            OsuHitSample(normal_set, addition_set, index, volume, filename))
-                    )
-                # x,y,time,type,hitSound,curveType|curvePoints,slides,length,edgeSounds,edgeSets
-                # x,y,time,type,hitSound,curveType|curvePoints,slides,length,edgeSounds,edgeSets,hitSample
+                    if m2 is None:
+                        raise ChartParseError(line_num, f"Unparseable hit sample data '{line}'.")
+                    hit_sample = hit_sample_from_match(m2)
+                    chart.hit_objects.append(OsuHitCircle(x, y, time, object_type, hit_sound, hit_sample))
                 elif m := re.match(RE_SLIDER, line):
-                    # ignore for now
-                    splits = line.split(",")
-                    x = int(splits[0])
-                    y = int(splits[1])
-                    time = int(splits[2]) / 1000
-                    object_type = int(splits[3])
-                    hit_sound = int(splits[4])
-                    curve_part = splits[5]
-                    curve_splits = curve_part.split("|")
-                    curve_type = curve_splits[0]
-                    curve_points = [OsuPoint(float(p.split(":")[0]), float(p.split(":")[1])) for p in curve_splits[1:]]
-                    slides = int(splits[6])
-                    px_length = int(splits[7])
-                    edge_sounds = [int(e) for e in splits[8].split("|")]
-                    edge_sets = splits[9].split("|")
-                    hit_sample_data = splits[10] if len(splits) == 9 else None
-                    if hit_sample_data:
-                        m2 = re.search(RE_HIT_SAMPLE, hit_sample_data)
-                        normal_set = int(m2.group(1))
-                        addition_set = int(m2.group(2))
-                        index = int(m2.group(3))
-                        volume = int(m2.group(4))
-                        filename = m2.group(5) or None
-                        hit_sample = OsuHitSample(normal_set, addition_set, index, volume, filename)
-                    else:
-                        hit_sample = OsuHitSample()
-                    chart.hit_objects.append(
-                        OsuSlider(x, y, time, object_type, hit_sound,
-                        curve_type, curve_points, slides, px_length, edge_sounds, edge_sets, hit_sample)
-                    )
-                # MAKING THE ASSUMPTION THESE REQUIRE HITSAMPLE
-                # OTHERWISE IT WOULD BE INDESCERNABLE FROM A HOLD
-                # x,y,time,type,hitSound,endTime,hitSample
+                    x, y, time, object_type, hit_sound = get_standard_data_from_match(m)
+                    # Slider specific
+                    slider_type = m.group(6)
+                    raw_curve_points = m.group(7)
+                    raw_curve_points = raw_curve_points.split("|")
+                    raw_curve_points = [s.split(":") for s in raw_curve_points]
+                    curve_points = []
+                    for p in raw_curve_points:
+                        curve_points.append(OsuPoint(int(p[0]), int(p[1])))
+                    slides = int(m.group(8))
+                    length = float(m.group(9))
+                    # Hit sample
+                    hit_sample_data = m.group(10)
+                    m2 = re.search(RE_HIT_SAMPLE, hit_sample_data)
+                    if m2 is None:
+                        raise ChartParseError(line_num, f"Unparseable hit sample data '{line}'.")
+                    hit_sample = hit_sample_from_match(m2)
+                    slider = OsuSlider(x, y, time, object_type, hit_sound,
+                                       slider_type, curve_points, slides, length, [], [], hit_sample)
+                    chart.hit_objects.append(slider)
+
                 elif m := re.match(RE_SPINNER, line):
-                    # ignore for now
-                    x = int(m.group(1))
-                    y = int(m.group(2))
-                    time = int(m.group(3)) / 1000
-                    object_type = int(m.group(4))
-                    hit_sound = int(m.group(5))
+                    x, y, time, object_type, hit_sound = get_standard_data_from_match(m)
                     end_time = int(m.group(6)) / 1000
+                    # Hit sample
                     hit_sample_data = m.group(7)
                     m2 = re.search(RE_HIT_SAMPLE, hit_sample_data)
-                    normal_set = int(m2.group(1))
-                    addition_set = int(m2.group(2))
-                    index = int(m2.group(3))
-                    volume = int(m2.group(4))
-                    filename = m2.group(5) or None
-                    hit_sample = OsuHitSample(normal_set, addition_set, index, volume, filename)
-                    chart.hit_objects.append(
-                        OsuSpinner(x, y, time, object_type, end_time, hit_sound, hit_sample)
-                    )
+                    if m2 is None:
+                        raise ChartParseError(line_num, f"Unparseable hit sample data '{line}'.")
+                    hit_sample = hit_sample_from_match(m2)
+                    chart.hit_objects.append(OsuSpinner(x, y, time, object_type, hit_sound, end_time, hit_sample))
                 else:
                     raise ChartParseError(line_num, f"Unknown hit object '{line}'.")
             else:
                 raise ChartParseError(line_num, f"Unknown header '{current_header}'.")
 
+        for ho in chart.hit_objects:
+            if isinstance(ho, OsuSlider):
+                ho.end_time = chart.calculate_slider_length(ho) + ho.time
+
         return chart
 
-    def calculate_slider_length(self, time: Seconds, slider: OsuSlider) -> Seconds:
-        latest_timing_point = [t for t in self.timing_points if t.time <= time][-1]
+    def calculate_slider_length(self, slider: OsuSlider) -> Seconds:
+        latest_timing_point = [t for t in self.timing_points if t.time <= slider.time][-1]
         return (slider.px_length / (self.difficulty.slider_multiplier * 100 * latest_timing_point.slider_velocity_multipler) * latest_timing_point.beat_length) * slider.slides
