140e67d3c88e612390919c2c31530d4157c5115f
DigiDuncan <digiduncan@gmail.com>
2024-01-20 00:26:39 -0500

    batching!

--- charm/objects/lyric_animator.py
+++ charm/objects/lyric_animator.py
@@ -2,6 +2,7 @@ from copy import copy
 from dataclasses import dataclass, field
 
 import arcade
+import pyglet
 
 Seconds = float
 
@@ -13,6 +14,7 @@ class LyricEvent:
     karaoke: str = ""
 
     _labels: list[arcade.Text] = field(default_factory=list)
+    _batch: pyglet.graphics.Batch = field(default_factory=pyglet.graphics.Batch)
 
     @property
     def end_time(self) -> Seconds:
@@ -24,18 +26,17 @@ class LyricEvent:
 
     def get_labels(self, x: float, y: float) -> arcade.Text:
         if not self._labels:
-            label_shadow = arcade.Text(self.text, x + 2, y - 2, font_name = "bananaslip plus", font_size = 24, color = (127, 127, 127, 255), align = "center", anchor_x = "center")
-            label_under = arcade.Text(self.text, x, y, font_name = "bananaslip plus", font_size = 24, color = (0, 0, 0, 255), align = "center", anchor_x = "center")
+            label_shadow = arcade.Text(self.text, x + 2, y - 2, font_name = "bananaslip plus", font_size = 24, color = (0, 0, 0, 127), align = "center", anchor_x = "center", batch = self._batch)
+            label_under = arcade.Text(self.text, x, y, font_name = "bananaslip plus", font_size = 24, color = (0, 0, 0, 255), align = "center", anchor_x = "center", batch = self._batch)
             self._labels.append(label_shadow)
             self._labels.append(label_under)
             if self.karaoke:
-                label_over = arcade.Text(self.karaoke, label_under.left, y, font_name = "bananaslip plus", font_size = 24, color = (255, 255, 0, 255), align = "left", anchor_x = "left")
+                label_over = arcade.Text(self.karaoke, label_under.left, y, font_name = "bananaslip plus", font_size = 24, color = (255, 255, 0, 255), align = "left", anchor_x = "left", batch = self._batch)
                 self._labels.append(label_over)
         return self._labels
 
     def draw(self):
-        for l in self._labels:
-            l.draw()
+        self._batch.draw()
 
 class LyricAnimator:
     def __init__(self, x: float, y: float, events: list[LyricEvent] = None) -> None:
