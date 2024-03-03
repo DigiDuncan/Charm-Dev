9fd5a1821dd51bc29ca72b9afa14a3e04a9237a3
DigiDuncan <digiduncan@gmail.com>
2024-03-03 13:42:26 -0500

    make paths if they're not

--- charm/lib/paths.py
+++ charm/lib/paths.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import appdirs
 
 
-def getDataDir():
+def _get_data_dir():
     appname = "Charm"
     appauthor = "DigiDuncan"
     datadir = Path(appdirs.user_data_dir(appname, appauthor))
@@ -11,7 +11,20 @@ def getDataDir():
 
 
 # File paths
-datadir = getDataDir()
+datadir = _get_data_dir()
 confpath = datadir / "charm.conf"
 songspath = datadir / "songs"
 scorespath = datadir / "scores.db"
+
+fnfpath = songspath / "fnf"
+fourkeypath = songspath / "4k"
+osupath = songspath / "osu"
+chpath = songspath / "ch"
+
+datadir.mkdir(parents=True, exist_ok=True)
+songspath.mkdir(parents=True, exist_ok=True)
+scorespath.mkdir(parents=True, exist_ok=True)
+fnfpath.mkdir(parents=True, exist_ok=True)
+fourkeypath.mkdir(parents=True, exist_ok=True)
+osupath.mkdir(parents=True, exist_ok=True)
+chpath.mkdir(parents=True, exist_ok=True)
