0af50397ba51ed44a8faa00a1d955ced6d8d656b
DragonMoffon <happydog.aja@gmail.com>
2024-07-25 23:23:18 +1200

    element docs

--- charm/ui/menu_list/song_element.py
+++ charm/ui/menu_list/song_element.py
@@ -4,12 +4,15 @@ from charm.lib.mini_mint import Element
 
 
 class ChartElement(Element[Element | None, Element]):
+    # displays info about a specific chart and are spawned in the sublist of the SongListElement
     pass
 
 
 class SongElement(Element[Element | None, Element]):
+    # displays info about a specific song and are spawned by the SongListElement directly
     pass
 
 
 class SongListElement(Element[Element | None, Element]):
+    # Holds a SongElement and a sublist of ChartElemetns and manages how and when the sublist appears
     pass
