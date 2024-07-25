8cc7c6e9841f339c4309d18ff48eb8340efe7a34
DragonMoffon <happydog.aja@gmail.com>
2024-07-25 23:18:04 +1200

    add nuitka config to main

--- charm/__main__.py
+++ charm/__main__.py
@@ -1,19 +1,10 @@
+# nuitka-project: --include-package-data=charm
+# nuitka-project: --windows-icon-from-ico=charm\data\images\charm-icon32t.png
+# nuitka-project: --force-stderr-spec=err.txt
+# nuitka-project: --windows-console-mode=disable
+# nuitka-project: --product-name=Charm
+# nuitka-project: --product-version=0.0.0.1
 from charm import main
 
-# -- TEMP --
-import arrow
-import cProfile
-import pstats
-
 if __name__ == "__main__":
-    launch_time = arrow.now()
     main.main()
-
-    # with cProfile.Profile() as pr:
-    #     main.main()
-
-    # with open(f'./debug/launch_{launch_time.format("YYYY-MM-DD_HH-mm-ss")}.prof', 'w') as output:
-    #     stats = pstats.Stats(pr, stream=output)
-    #     stats.sort_stats('cumulative')
-    #     stats.print_stats()
-    # main.main()
