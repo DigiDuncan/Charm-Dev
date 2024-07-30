27736de3332233853a4e2f5c957a1baf16593f57
DigiDuncan <digiduncan@gmail.com>
2024-07-30 14:02:39 -0400

    osu parsers

---
+++ charm/refactor/parsers/mania.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from charm.lib.gamemodes.osu import OsuHold
+from charm.refactor.charts.fourkey import FourKeyNote, FourKeyChart
+from charm.refactor.generic.parser import Parser
+from charm.refactor.parsers._osu import OsuHitCircle, RawOsuChart
+
+
+class ManiaParser(Parser[FourKeyChart]):
+    def __init__(self, path: Path):
+        """A four-key song from osu!mania."""
+        super().__init__(path)
+
+    @classmethod
+    def parse(cls, path: Path) -> list[FourKeyChart]:
+        chart_files = path.glob("*.osu")
+        charts = []
+
+        added_bpm_events = False
+
+        for p in chart_files:
+            raw_chart = RawOsuChart.parse(p)  # SO MUCH is hidden by this function
+            chart = FourKeyChart(raw_chart.metadata.difficulty, None)
+            # !: Removed this! Is it even used? If it is, aaa!
+            # if not added_bpm_events:
+            #     song.events.extend(raw_chart.timing_points)
+            #     added_bpm_events = True
+            for hit_object in raw_chart.hit_objects:
+                if isinstance(hit_object, OsuHitCircle):
+                    chart.notes.append(FourKeyNote(chart, hit_object.time, hit_object.get_lane(4)))
+                elif isinstance(hit_object, OsuHold):
+                    chart.notes.append(FourKeyNote(chart, hit_object.time, hit_object.get_lane(4), length = hit_object.length))
+            charts.append(chart)
+        # TODO: Handle no charts
+        return charts
---
+++ charm/refactor/parsers/taiko.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from charm.refactor.generic.parser import Parser
+from charm.refactor.charts.taiko import TaikoNote, TaikoChart, TaikoNoteType
+from charm.refactor.parsers._osu import OsuHitCircle, OsuSlider, OsuSpinner, RawOsuChart
+
+class TaikoParser(Parser[TaikoChart]):
+    def __init__(self, path: Path):
+        super().__init__(path)
+
+    def parse(self) -> list[TaikoChart]:
+        chart_files = self.path.glob("*.osu")
+        charts = []
+
+        added_bpm_events = False
+
+        for p in chart_files:
+            raw_chart = RawOsuChart.parse(p)  # SO MUCH is hidden by this function
+            chart = TaikoChart(raw_chart.metadata.difficulty, None)
+            # !: Removed this! Is it even used? If it is, aaa!
+            # if not added_bpm_events:
+            #     song.events.extend(raw_chart.timing_points)
+            #     added_bpm_events = True
+            for hit_object in raw_chart.hit_objects:
+                if isinstance(hit_object, OsuHitCircle):
+                    if hit_object.taiko_kat:
+                        chart.notes.append(TaikoNote(chart, hit_object.time, 0, 0, TaikoNoteType.KAT, large = hit_object.taiko_large))
+                    else:
+                        chart.notes.append(TaikoNote(chart, hit_object.time, 0, 0, TaikoNoteType.DON, large = hit_object.taiko_large))
+                elif isinstance(hit_object, OsuSlider):
+                    chart.notes.append(TaikoNote(chart, hit_object.time, 0, hit_object.length, TaikoNoteType.DRUMROLL, large = hit_object.taiko_large))
+                elif isinstance(hit_object, OsuSpinner):
+                    chart.notes.append(TaikoNote(chart, hit_object.time, 0, hit_object.length, TaikoNoteType.DENDEN, large = hit_object.taiko_large))
+            charts.append(chart)
+        # TODO: Handle no charts
+        return charts
