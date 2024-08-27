58e350f46eea345d27257898cb835935956132ad
DigiDuncan <digiduncan@gmail.com>
2024-08-27 01:45:30 -0400

    get V2 working in the menu
    THIS IS POSSIBLY CURSED. @DragonMoffon, if you hate this, please undo it!

--- charm/refactor/loading.py
+++ charm/refactor/loading.py
@@ -8,7 +8,9 @@
 #
 #   ! This is currently done per gamemode folder, but if a mod
 #   ! in the future wants to mix gamemodes this will need updating.
+import json
 from typing import NamedTuple
+from collections.abc import Callable
 from pathlib import Path
 import os
 
@@ -20,40 +22,70 @@ from charm.refactor.generic.chart import Chart, ChartMetadata
 # -- PARSERS --
 from charm.refactor.generic.parser import Parser
 from charm.refactor.parsers.fnf import FNFParser
+from charm.refactor.parsers.fnfv2 import FNFV2Parser
 from charm.refactor.parsers.mania import ManiaParser
 from charm.refactor.parsers.sm import SMParser
 from charm.refactor.parsers.hero import HeroParser
 from charm.refactor.parsers.taiko import TaikoParser
 
+ParserChooser = Callable[[Path], bool]
+
+def get_FNF_version(p: Path) -> int:
+    if p.suffix != ".json":
+        return 0
+    else:
+        with open(p, encoding = "utf-8") as f:
+            try:
+                j = json.load(f)
+            except json.JSONDecodeError:
+                return 0
+            if "version" not in j:
+                return 1
+            else:
+                try:
+                    v = int(j["version"].split(".")[0])
+                    return v
+                except ValueError:
+                    return 1
+
 class TypePair(NamedTuple):
     gamemode: str
-    filetype: str
+    filetype: str | ParserChooser
 
+# TODO: Parse MIDI
 parser_map: dict[TypePair, type[Parser]] = {
-    TypePair('fnf', '.json'): FNFParser,
+    TypePair('fnf', lambda p: get_FNF_version(p) == 1): FNFParser,
+    TypePair('fnf', lambda p: get_FNF_version(p) == 2): FNFV2Parser,
     TypePair('4k', '.osu'): ManiaParser,
     TypePair('4k', '.ssc'): SMParser,
     TypePair('4k', '.sm'): SMParser,
     TypePair('hero', '.chart'): HeroParser,
     TypePair('taiko', '.osu'): TaikoParser
 }
-# TODO: parse midi
-# ! TODO: Good luck Dragon, two Parsers use .json now
 
+def get_needed_parsers(directory: Path, files: list[str]) -> set[Parser]:
+    pairs = tuple(parser_map.items())
+    found_parsers = []
+    for typepair, parser in pairs:
+        if isinstance(typepair.filetype, str):
+            if any(file.endswith(typepair.filetype) for file in files):
+                found_parsers.append(parser)
+        else:
+            if any(typepair.filetype(directory / file) for file in files):
+                found_parsers.append(parser)
+    return set(found_parsers)
 
 def load_chartsets() -> list[ChartSet]:
-    pairs = tuple(parser_map.items())
     all_set_paths = (p for p in songspath.glob('**/*') if p.is_dir())
     chartsets = []
     for d in all_set_paths:
         files = [f for f in os.listdir(d) if Path.is_file(d / f)]
         if not files:
            continue
-        needed_parsers = {parser for typepair, parser in pairs if any(file.endswith(typepair.filetype) for file in files)}
+        needed_parsers = get_needed_parsers(d, files)
         charts = []
         for parser in needed_parsers:
             charts.extend(parser.parse_metadata(d))
-
         if charts:
             chartset = ChartSet(d)
             chartset.charts = charts
@@ -61,10 +93,7 @@ def load_chartsets() -> list[ChartSet]:
             chartsets.append(chartset)
     return chartsets
 
-
 def load_chart(chart_metadata: ChartMetadata) -> list[Chart]:
-    gamemode = chart_metadata.gamemode
-    filetype = chart_metadata.path.suffix
-
-    parser = parser_map[TypePair(gamemode, filetype)]
+    parsers = get_needed_parsers(chart_metadata.path.parent, [str(chart_metadata.path.relative_to(chart_metadata.path.parent))])
+    parser = next(iter(parsers))
     return parser.parse_chart(chart_metadata)
