1bf561025a2c1f8eba175cb9e35a42831870c6b1
DragonMoffon <happydog.aja@gmail.com>
2024-09-07 12:26:20 +1200

    update scott list

--- charm/core/gamemodes/fnf/highway.py
+++ charm/core/gamemodes/fnf/highway.py
@@ -2,8 +2,11 @@
 # It's here because there's currently no gamemode definitions
 # so it's unclear why FNFParser -> FourKeyHighway.
 
+from charm.core.gamemodes.fnf.chart import FNFChart
 from charm.core.gamemodes.four_key.highway import FourKeyHighway
+from charm.core.generic.engine import Engine
 
 
 class FNFHighway(FourKeyHighway):
-    pass
+    def __init__(self, chart: FNFChart, engine: Engine, pos: tuple[int, int], size: tuple[int, int] | None = None, gap: int = 5):
+        super().__init__(chart, engine, pos, size, gap)  # type: ignore reportArgumentType
