aeaa2a94c182661de1c8e5c752c19e21b7ce486b
Natalie Fearnley <nfearnley@gmail.com>
2024-09-09 21:29:55 -0400

    Properly typed recursive annotation

--- charm/ui/settings/settings_tab.py
+++ charm/ui/settings/settings_tab.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 from charm.lib.mini_mint import Element
 
+Elements = Element["Elements"]
 
-class SettingsTabElement(Element[Element]):
+class SettingsTabElement(Elements):
     pass
