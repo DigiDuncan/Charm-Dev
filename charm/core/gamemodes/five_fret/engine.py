e1632da9ba633ffc23be751211facbb1a1ae52da
DragonMoffon <happydog.aja@gmail.com>
2024-09-22 14:35:46 +1200

    continuing five fret work

--- charm/core/gamemodes/five_fret/engine.py
+++ charm/core/gamemodes/five_fret/engine.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from queue import Queue
 
+from charm.lib.errors import TODOError
+
 from charm.core.generic.engine import DigitalKeyEvent
 from charm.lib.keymap import keymap
 from charm.lib.types import Seconds
@@ -15,6 +17,7 @@ class ChordShapeChangeEvent(EngineEvent):
         super().__init__(time)
         self.shape = chord_shape
 
+EMPTY_CHORD = ChordShape(False, False, False, False, False)
 
 class FiveFretEngine(Engine[FiveFretChart, FiveFretNote]):
     def __init__(self, chart: FiveFretChart, offset: Seconds = 0):
@@ -25,20 +28,47 @@ class FiveFretEngine(Engine[FiveFretChart, FiveFretNote]):
         super().__init__(chart, judgements, offset)
         self.current_notes: list[FiveFretChord] = self.chart.chords[:] # Override the default engine
 
-        self.chord_events: list[ChordShapeChangeEvent] = []
+        self.chord_events: Queue[ChordShapeChangeEvent] = Queue()
         self.strum_events: Queue[DigitalKeyEvent] = Queue()
 
         self.last_event_times = defaultdict(None)
-        self.last_chord_shape: ChordShape = ChordShape(False, False, False, False, False)
+        self.last_chord_shape: ChordShape = EMPTY_CHORD
 
         self.infinite_front_end = False
         self.hopo_leniency: Seconds = 0.080
         self.strum_leniency: Seconds = 0.060
         self.no_note_leniency: Seconds = 0.030
+        self.sustain_end_leniency: Seconds = 0.01
 
         self.strum_time: Seconds = -float('inf')
 
         # todo: ignore overstrum during countdown events
+
+    @property
+    def window_front_end(self) -> Seconds:
+        return self.chart_time + self.hit_window
+
+    @property
+    def window_back_end(self) -> Seconds:
+        return self.chart_time - self.hit_window
+
+    def on_strum(self):
+        # When a player strums a few things can occur:
+        # 1) No strum has happened recently
+        #   1 a) No notes are around for it to be a valid strum
+        #   1 b) Add the strum to the queue to be processed next update
+        # 2) A strum has happened recently -> Mark it as overstrum
+        # 3) We are in a countdown so ignore it
+        pass
+
+    def on_chord_change(self):
+        pass
+
+    def on_whammy(self):
+        raise TODOError('Dragon/Digi needs to add whammy')
+
+    def on_starpower(self):
+        raise TODOError('Dragon/Digi needs to do Starpower')
 
     def on_key_press(self, symbol: int, modifiers: int) -> None:
         # ignore spam during front/back porch
@@ -60,14 +90,14 @@ class FiveFretEngine(Engine[FiveFretChart, FiveFretNote]):
                 keymap.hero.blue.held,
                 keymap.hero.orange.held
             )
-            self.chord_events.append(ChordShapeChangeEvent(t, shape))
+            self.chord_events.put_nowait(ChordShapeChangeEvent(t, shape))
             self.last_chord_shape = shape
             self.last_event_times[current_action] = t
         elif current_action == keymap.hero.strumup:
-            self.strum_events.put(DigitalKeyEvent(t, "strumup", "down"))
+            self.strum_events.put_nowait(DigitalKeyEvent(t, "strumup", "down"))
             self.last_event_times['strum'] = t
         elif current_action == keymap.hero.strumdown:
-            self.strum_events.put(DigitalKeyEvent(t, "strumdown", "down"))
+            self.strum_events.put_nowait(DigitalKeyEvent(t, "strumdown", "down"))
             self.last_event_times['strum'] = t
 
     def on_key_release(self, symbol: int, modifiers: int) -> None:
@@ -82,7 +112,7 @@ class FiveFretEngine(Engine[FiveFretChart, FiveFretNote]):
             return
 
         if current_action in {keymap.hero.green, keymap.hero.red, keymap.hero.yellow, keymap.hero.blue, keymap.hero.orange}:
-            self.chord_events.append(ChordShapeChangeEvent(self.chart_time, ChordShape(
+            self.chord_events.put_nowait(ChordShapeChangeEvent(self.chart_time, ChordShape(
                 keymap.hero.green.held,
                 keymap.hero.red.held,
                 keymap.hero.yellow.held,
@@ -100,18 +130,18 @@ class FiveFretEngine(Engine[FiveFretChart, FiveFretNote]):
 
     def calculate_score(self) -> None:
         for chord in self.current_notes[:]:
-            if self.chart_time + self.hit_window < chord.time:
+            if self.window_front_end < chord.time:
+                # If the chord is outside the hit window then we have processed all hit able notes
                 break
 
-            if self.chart_time - self.hit_window > chord.time:
+            if self.window_back_end > chord.time:
+                # If the note has left the hit window without being it then it is missed
                 chord.missed = True
                 chord.hit_time = float('inf')
                 self.score_chord(chord)
                 self.current_notes.remove(chord)
             else:
-                for chord_event in self.chord_events:
-                    if chord.shape == chord_event.shape:
-                        pass
+                pass
 
 
     def score_chord(self, chord: FiveFretChord) -> None:
@@ -123,7 +153,8 @@ class FiveFretEngine(Engine[FiveFretChart, FiveFretNote]):
     def score_sustain(self, note: FiveFretNote) -> None:
         raise NotImplementedError
 
-
+    def overstrum(self) -> None:
+        pass
 
     # def generate_results(self) -> Results[C]:
     #     raise NotImplementedError
